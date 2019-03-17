875bd76f040c07614deba17330ce56ce1f863a4e
MartinAlegria <A01022216@itesm.mx>
2019-03-17 15:02:17 -0600

    Gauss Elimination almost done

---
+++ Segundo_Parcial/second_partial.f90
@@ -0,0 +1,92 @@
+PROGRAM SECOND_PARTIAL
+
+	IMPLICIT NONE
+
+	CALL gauss_elimination()
+
+END PROGRAM SECOND_PARTIAL
+
+!********** System of Linear Equations **********!
+
+SUBROUTINE gauss_elimination()
+	IMPLICIT NONE
+	!********** READ FILE **********!
+	INTEGER :: n, n_1,i,j,k,l 
+	DOUBLE PRECISION, dimension (:,:), allocatable :: matrix
+	DOUBLE PRECISION, dimension (:,:), allocatable :: calcs
+	DOUBLE PRECISION, dimension (:), allocatable :: results
+	DOUBLE PRECISION, dimension (:), allocatable :: temps
+
+	open(unit = 10, file = "test.txt")
+	read(10,*)n
+	write(*,*)n
+	n_1 = n+1
+	allocate ( matrix(n,n_1) )
+	allocate ( calcs(n,n_1) )
+	allocate ( results(n) )   
+	allocate ( temps(n) )  
+	do i=1,n
+		read(10,*)matrix(i,:)
+		write(*,*)matrix(i,:)
+	enddo
+
+	calcs = matrix
+	close(10) 
+	!********** READ FILE **********!
+
+
+	!********** TURN MATRIX INTO LOWER TRIANGLE MATRIX **********!
+	do i=1,n-1
+		matrix(i,:) = matrix(i,:)/calcs(i,i)
+		calcs = matrix
+		do j=i+1,n
+			calcs = matrix
+			do k=1,n+1
+				matrix(j,k) = ((-1)*calcs(j,i) * matrix(i,k)) + matrix(j,k)
+			enddo
+		enddo
+		calcs = matrix
+		write(*,*) "------------------- New matrix -------------------"
+		do l=1,n
+			write(*,*)matrix(l,:)
+		enddo
+	enddo
+
+	write(*,*) "-------------------Final matrix -------------------"
+	do i=1,n
+			write(*,*)matrix(i,:)
+	enddo
+	!********** TURN MATRIX INTO LOWER TRIANGLE MATRIX **********!
+
+	!********** BACKWARDS SUBSTITUTION **********!
+	temps(n) = temps(n) + (matrix(n,n_1)/matrix(n,n)) !--- Get Xsub1 first
+	write(*,*) "Xsub1 = ", temps(n)
+	do i=n,1, -1
+
+	
+	enddo
+	!********** BACKWARDS SUBSTITUTION **********!
+
+END SUBROUTINE gauss_elimination
+
+SUBROUTINE LU_decomp()
+END SUBROUTINE LU_decomp
+
+SUBROUTINE gauss_seidel()
+END SUBROUTINE gauss_seidel
+
+!********** Interpolation **********!
+
+SUBROUTINE power_series()
+END SUBROUTINE power_series
+
+SUBROUTINE lagrange()
+END SUBROUTINE lagrange
+
+SUBROUTINE newton()
+END SUBROUTINE newton
+
+!********* FILE ************!
+
+
+
