3c5ab1aea2c2fc441c87dcf76aa0f5fe048219e6
MartinAlegria <A01022216@itesm.mx>
2019-03-17 21:46:09 -0600

    LU Decomposition done

--- Segundo_Parcial/second_partial.f90
+++ Segundo_Parcial/second_partial.f90
@@ -1,8 +1,8 @@
 PROGRAM SECOND_PARTIAL
 
 	IMPLICIT NONE
-
 	CALL gauss_elimination()
+	CALL lu_decomp()
 
 END PROGRAM SECOND_PARTIAL
 
@@ -69,7 +69,7 @@ SUBROUTINE gauss_elimination()
 	enddo
 
 	!********** EXPORT TO CSV **********!
-	open (unit = 2, file = "results.csv")
+	open (unit = 2, file = "gauss_elimination.csv")
 	write(2,*)"Xsub",(",",j,j=1,n)
 	write(2,*)"-", (",",results(i), i=1,n)
 	close(2)
@@ -79,8 +79,106 @@ SUBROUTINE gauss_elimination()
 
 END SUBROUTINE gauss_elimination
 
-SUBROUTINE LU_decomp()
-END SUBROUTINE LU_decomp
+SUBROUTINE lu_decomp()
+		!********** READ FILE **********!
+	INTEGER :: n, n_1,i,j,k,l 
+	DOUBLE PRECISION, dimension (:,:), allocatable :: matrix
+	DOUBLE PRECISION, dimension (:,:), allocatable :: lm
+	DOUBLE PRECISION, dimension (:,:), allocatable :: um
+	DOUBLE PRECISION, dimension (:), allocatable :: y
+	DOUBLE PRECISION, dimension (:), allocatable :: x
+	DOUBLE PRECISION, dimension (:), allocatable :: b
+	DOUBLE PRECISION :: temps
+
+	open(unit = 10, file = "test.txt")
+	read(10,*)n
+	n_1 = n+1
+	allocate ( matrix(n,n) )
+	allocate ( lm(n,n) ) 
+	allocate ( um(n,n) ) 
+	allocate ( y(n) )
+	allocate ( x(n) )
+	allocate ( b(n) )
+
+	write(*,*) "************* Read Matrix *************"   
+	do i=1,n
+		read(10,*)matrix(i,:),b(i)
+		write(*,*)matrix(i,:)
+	enddo
+
+ 
+	close(10)
+	!********** READ FILE **********!
+
+	lm(:,1) = matrix(:,1)
+	um(1,:) = matrix(1,:)/lm(1,1)
+	um(1,1) = 1;
+
+	do k=2,n
+		do j=2,n
+			
+			do i=j,n
+					lm(i,j) = matrix(i,j) - dot_product(lm(i,1:j-1), um(1:j-1,j))
+			enddo
+			um(k,j) = (matrix(k,j) - dot_product(lm(k,1:k-1), um(1:k-1,j)) )/lm(k,k)
+		enddo
+	enddo
+
+	do i=n-1,1,-1
+		do j=n,i+1,-1
+			lm(i,j) = 0
+		enddo
+	enddo
+
+	do i=2,n
+		do j=1,i-1
+			um(i,j)=0
+		enddo
+	enddo
+
+	write(*,*) "************* L matrix *************"
+	do i=1,n
+		write(*,*)lm(i,:)
+	enddo
+	write(*,*) "************* U matrix *************"
+	do i=1,n
+		write(*,*)um(i,:)
+	enddo
+	write(*,*) "************* B matrix *************"
+	write(*,*) b(:)
+
+	! ******* FOWARD SUBSTITUTION
+	do i=1,n
+		temps = b(i)
+		do j=1,i-1
+			temps = temps - lm(i,j)*y(j)
+		enddo
+		y(i) = temps/lm(i,i)
+	enddo
+
+	write(*,*) "Y MATRIX *********************"
+	write(*,*) y(:)
+
+	x(n) = (y(n)/um(n,n))!--- Get Xsub1 first
+	write(*,*) x(n)
+	do i=n-1, 1, -1
+		temps = 0
+		do j=i+1, n
+			temps = temps + um(i,j)*x(j)
+		enddo
+		x(i) = (y(i) - temps)/um(i,i)
+	enddo
+
+
+	!********** EXPORT TO CSV **********!
+	open (unit = 2, file = "lu_decomp.csv")
+	write(2,*)"Xsub",(",",j,j=1,n)
+	write(2,*)"-", (",",x(i), i=1,n)
+	close(2)
+	!********** EXPORT TO CSV **********!
+
+
+END SUBROUTINE lu_decomp
 
 SUBROUTINE gauss_seidel()
 END SUBROUTINE gauss_seidel
