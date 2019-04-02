c7c70fdcb3ea2ae3257ae753c04da32bb701bb57
MartinAlegria <A01022216@itesm.mx>
2019-04-01 22:32:11 -0600

    Gauss-Seidel done

--- Segundo_Parcial/second_partial.f90
+++ Segundo_Parcial/second_partial.f90
@@ -2,7 +2,9 @@ PROGRAM SECOND_PARTIAL
 
 	IMPLICIT NONE
 	CALL gauss_elimination()
-	CALL lu_decomp()
+	!CALL lu_decomp()
+	CALL gauss_seidel()
+
 
 END PROGRAM SECOND_PARTIAL
 
@@ -75,12 +77,11 @@ SUBROUTINE gauss_elimination()
 	close(2)
 	!********** EXPORT TO CSV **********!
 
-	!********** BACKWARDS SUBSTITUTION **********!
-
+	!********** BACKWARDS SUBSTITUTION **********! !*********** DONE ***********!
 END SUBROUTINE gauss_elimination
 
 SUBROUTINE lu_decomp()
-		!********** READ FILE **********!
+	!********** READ FILE **********!
 	INTEGER :: n, n_1,i,j,k,l 
 	DOUBLE PRECISION, dimension (:,:), allocatable :: matrix
 	DOUBLE PRECISION, dimension (:,:), allocatable :: lm
@@ -175,12 +176,75 @@ SUBROUTINE lu_decomp()
 	write(2,*)"Xsub",(",",j,j=1,n)
 	write(2,*)"-", (",",x(i), i=1,n)
 	close(2)
-	!********** EXPORT TO CSV **********!
-
-
+	!********** EXPORT TO CSV **********! !*********** DONE ***********!
 END SUBROUTINE lu_decomp
 
 SUBROUTINE gauss_seidel()
+
+	!********** READ FILE **********!
+	INTEGER :: n, n_1,i,j,k,l 
+	DOUBLE PRECISION, dimension (:,:), allocatable :: matrix
+	DOUBLE PRECISION, dimension (:), allocatable :: x
+	DOUBLE PRECISION, dimension (:), allocatable :: past_x
+	DOUBLE PRECISION, dimension (:), allocatable :: b
+	DOUBLE PRECISION :: temps, sum, error
+
+	open(unit = 10, file = "test.txt")
+	read(10,*)n
+	n_1 = n+1
+	allocate ( matrix(n,n) )
+	allocate ( x(n) )
+	allocate ( b(n) )
+
+	write(*,*) "************* Read Matrix *************"   
+	do i=1,n
+		read(10,*)matrix(i,:),b(i)
+		write(*,*)matrix(i,:)
+	end do
+
+	x(:) = 0
+
+	do
+
+	!********** EVALUATE EQUATIONS **********!
+	do i=1,n
+		write(*,*) "************* for 1 *************" 
+		temps = 0
+		past_x = x
+		do j=1,n
+			write(*,*) "************* for 2 *************" 
+			if (j /= i) then
+				temps = temps + matrix(i,j)*x(j)
+			endif
+		end do
+		x(i) = (b(i) - temps)/matrix(i,i)
+	end do
+
+	!********** EVALUATE EQUATIONS **********!
+
+	write(*,*) x(:)
+
+	!********** CALCULATE AND COMPARE ERRORS **********!
+	sum = 0
+	do i=1,n
+		error = x(i) - past_x(i)
+		sum = sum + error
+	enddo
+
+	write(*,*) sum
+
+	if (abs(sum)<= 0.0001) then
+		EXIT
+	endif
+
+	!********** CALCULATE AND COMPARE ERRORS **********!
+
+ 	end do
+
+ 	write(*,*) "@@@@ FINAL -------", x(:)
+
+	close(10)
+	!********** READ FILE **********!
 END SUBROUTINE gauss_seidel
 
 !********** Interpolation **********!
